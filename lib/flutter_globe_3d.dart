d1e58b6f7fd0159418e479833804fbd655d7b872
浙江军阀张作霖 <>
2025-12-06 21:46:16 +0800

    文件修复

--- lib/flutter_globe_3d.dart
+++ lib/flutter_globe_3d.dart
@@ -1,7 +1,7 @@
-library;
+library flutter_globe_3d;
 
 // Core
 export 'src/earthglobe.dart' show Flutter3DGlobe;
 export 'src/earth_controller.dart' show EarthController, EarthConfig;
 export 'src/models/earth_connection.dart' show EarthConnection;
-export 'src/models/earth_maker.dart';
+export 'src/models/earth_maker.dart' show EarthMarker;
