bef7585e11d75e7e81593a47c591b8e2dc245687
浙江军阀张作霖 <>
2025-12-06 22:30:45 +0800

    制作演示照片，修改example

--- example/main.dart
+++ example/main.dart
@@ -1,23 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_globe_3d/src/earth_controller.dart';
-import 'package:flutter_globe_3d/src/earthglobe.dart';
+import 'package:flutter_globe_3d/flutter_globe_3d.dart';
 
-class Example extends StatefulWidget {
-  const Example({super.key});
+class MyGlobeApp extends StatefulWidget {
+  const MyGlobeApp({super.key});
 
   @override
-  State<Example> createState() => _ExampleState();
+  State<MyGlobeApp> createState() => _MyGlobeAppState();
 }
 
-class _ExampleState extends State<Example> {
-  EarthController earthController = EarthController();
+class _MyGlobeAppState extends State<MyGlobeApp> {
+  late EarthController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = EarthController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Flutter3DGlobe(
-      controller: earthController,
-      radius: 200,
-      texture: AssetImage("assets/example.png"),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(height: 100),
+          Container(
+            decoration: BoxDecoration(color: Colors.black),
+            child: Row(
+              children: [
+                Flutter3DGlobe(
+                  controller: _controller,
+                  texture: AssetImage('assets/mark.jpg'),
+                  radius: 150,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "Spark",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
